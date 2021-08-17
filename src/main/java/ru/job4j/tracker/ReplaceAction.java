e1e534243469a2ff7088a24af07fab3632a9df00
bohdan <bshevyrov@gmail.com>
2021-08-17 17:56:54 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -14,15 +14,21 @@ public class ReplaceAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        out.println("=== Edit item ====");
-        int id = input.askInt("Enter id: ");
-        String name = input.askStr("Enter name : ");
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            out.println("Заяка изменена успешно.");
-        } else {
-            out.println("Ошибка замены заявки.");
-        }
+        do {
+            out.println("=== Edit item ====");
+            int id = input.askInt("Enter id: ");
+            if (tracker.findById(id) == null) {
+                System.out.println("Please enter valid id.");
+                continue;
+            }
+            String name = input.askStr("Enter name : ");
+            Item item = new Item(name);
+            if (tracker.replace(id, item)) {
+                out.println("Заяка изменена успешно.");
+            } else {
+                out.println("Ошибка замены заявки.");
+            }
+        } while (false);
         return true;
     }
 }
